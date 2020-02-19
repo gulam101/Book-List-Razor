933e006d5a9da939c3281c8934d5d66c94ebd12a
Gulam Qasim <gfram@hotmail.co.uk>
2020-02-19 21:29:57 +0000

    Added Injection

--- BookListRazor/Pages/BookList/Index.cshtml.cs
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using BookListRazor.Model;
 
 namespace BookListRazor
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDb _db;
+
+        public IndexModel(ApplicationDb db)
+        {
+            _db = db;
+        }
         public void OnGet()
         {
 
