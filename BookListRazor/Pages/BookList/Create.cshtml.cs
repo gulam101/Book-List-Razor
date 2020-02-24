e80ae19a8e7a702f69239de26a00350499cc703d
Gulam Qasim <gfram@hotmail.co.uk>
2020-02-24 17:18:12 +0000

    Added New Razor Page

---
+++ BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using BookListRazor.Model;
+
+namespace BookListRazor
+{
+    public class CreateModel : PageModel
+    {
+
+        private readonly ApplicationDb _db;
+        public void OnGet()
+        {
+
+        }
+    }
+}
