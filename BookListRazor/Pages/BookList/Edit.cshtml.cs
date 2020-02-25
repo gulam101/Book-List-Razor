638b456285df437fab1225892c591133a3226305
Gulam Qasim <gfram@hotmail.co.uk>
2020-02-25 14:06:35 +0000

    Removed Unused Namespaces

--- BookListRazor/Pages/BookList/Edit.cshtml.cs
+++ BookListRazor/Pages/BookList/Edit.cshtml.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using BookListRazor.Model;
+﻿using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
 
 namespace BookListRazor
 {
