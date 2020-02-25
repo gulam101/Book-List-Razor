caa70fac8eb1ad23b60914c70eff1a21cd0cc19c
Gulam Qasim <gfram@hotmail.co.uk>
2020-02-25 14:22:11 +0000

    Updated Comments

--- BookListRazor/Pages/BookList/Create.cshtml.cs
+++ BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using BookListRazor.Model;
@@ -34,6 +31,7 @@ namespace BookListRazor
                 await _db.Book.AddAsync(book);
                 //Once this command is executed this will push data into the database
                 await _db.SaveChangesAsync();
+                //Returns to Index with updated information in database
                 return RedirectToPage("Index");
             } 
             else
